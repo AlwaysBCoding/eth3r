2f48968381b5238fe6a50941104062cba6df0914
Jordan Leigh <AlwaysBCoding@gmail.com>
2016-08-10 16:47:24 -0700

    better dashboard layout, stateful left nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import EthereumService from './services/EthereumService'
 import QRCode from 'qrcode.react'
-import './App.css'
+import './styles/App.css'
 
 class App extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
+      activeNavIdent: "wallet",
       walletCreated: false,
       walletPassword: "",
       walletPrivateKey: "",
@@ -28,46 +29,108 @@ class App extends Component {
   }
 
   render() {
-    var AppHeader, AppContent
+    var AppHeader, AppWrapper, AppNav, AppContent
 
     AppHeader =
     <div className="App-Header">
-      <h1>Eth3r Wallet</h1>
-      <input
-        className="wallet-password"
-        type="text"
-        value={this.state.walletPassword}
-        onChange={this._walletPasswordChange.bind(this)} />
-      <div
-        className="generate-wallet"
-        onClick={this._generateWallet.bind(this)}>
-        <p>{`Generate Wallet`}</p>
+      <h1>Eth3r Tools</h1>
+    </div>
+
+    AppNav =
+    <div className="App-Nav">
+      <div className={(this.state.activeNavIdent == "wallet")? "nav-item active" : "nav-item"}
+           data-nav-ident="wallet"
+           onClick={() => this.setState({activeNavIdent: "wallet"})}>
+        <p>Wallet</p>
+      </div>
+      <div className={(this.state.activeNavIdent == "transactions")? "nav-item active" : "nav-item"}
+           data-nav-ident="transactions"
+           onClick={() => this.setState({activeNavIdent: "transactions"})}>
+        <p>Transactions</p>
       </div>
     </div>
 
-    if(this.state.walletCreated) {
-      AppContent =
-      <div className="App-Content">
-        <div className="private-key">
-          <h1>Private Key</h1>
-          <p>{this.state.walletPrivateKey}</p>
-          <QRCode value={this.state.walletPrivateKey} />
+    switch(this.state.activeNavIdent) {
+      case "wallet":
+        var ContentHeader, Segment2
+
+        ConetentHeader =
+        <div className="Content-Header">
+          <h1>Create a Wallet</h1>
+          <input
+            className="wallet-password"
+            type="text"
+            value={this.state.walletPassword}
+            onChange={this._walletPasswordChange.bind(this)} />
+          <div
+            className="action-button"
+            onClick={this._generateWallet.bind(this)}>
+            <p>{`Generate Wallet`}</p>
+          </div>
         </div>
-        <div className="address">
-          <h1>Wallet Address</h1>
-          <p>{this.state.walletAddress}</p>
-          <QRCode value={this.state.walletAddress} />
+
+        if(this.state.walletCreated) {
+          Segment2 =
+          <div className="Content-Content">
+            <div className="private-key">
+              <h1>Private Key</h1>
+              <p>{this.state.walletPrivateKey}</p>
+              <QRCode value={this.state.walletPrivateKey} />
+            </div>
+            <div className="address">
+              <h1>Wallet Address</h1>
+              <p>{this.state.walletAddress}</p>
+              <QRCode value={this.state.walletAddress} />
+            </div>
+          </div>
+        } else {
+          Segment2 =
+          <div />
+        }
+
+        AppContent =
+        <div className="App-Content wallet">
+          {ConetentHeader}
+          {Segment2}
         </div>
-      </div>
-    } else {
-      AppContent =
-      <div />
+        break;
+      case "transactions":
+        var ConetentHeader
+        ConetentHeader =
+        <div className="Content-Header">
+          <h1>Create a Transaction</h1>
+          <input
+            className="wallet-password"
+            type="text"
+            value={this.state.walletPassword}
+            onChange={this._walletPasswordChange.bind(this)} />
+          <div
+            className="action-button"
+            onClick={this._generateWallet.bind(this)}>
+            <p>{`Create Transaction`}</p>
+          </div>
+        </div>
+
+        AppContent =
+        <div className="App-Content transaction">
+          {ConetentHeader}
+        </div>
+        break;
+      default:
+        AppContent =
+        <div />
     }
+
+    AppWrapper =
+    <div className="App-Wrapper">
+      {AppNav}
+      {AppContent}
+    </div>
 
     return (
       <div className="App">
         {AppHeader}
-        {AppContent}
+        {AppWrapper}
       </div>
     )
   }
